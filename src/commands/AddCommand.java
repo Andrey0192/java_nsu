bcb0fbddb35e024cde2e23cf2309ffd92c1ce995
bichevskiy <45871629+Andrey0192@users.noreply.github.com>
2025-06-11 19:25:02 +0700

    Add files via upload

--- src/commands/AddCommand.java
+++ src/commands/AddCommand.java
@@ -1,8 +1,5 @@
 package commands;
-import Exeptionals.CommandExecutionException;
-
-import java.util.HashMap;
-import java.util.Stack;
+import exceptions.CommandExecutionException;
 
 public class AddCommand extends AbstractCommand {
 
@@ -11,23 +8,19 @@ public class AddCommand extends AbstractCommand {
     }
 
     @Override
-    public void execute(Stack<Double> stack, HashMap<String, Double> definitions, String... args) {
-        try {
+    public void execute(ExecutionContext context, String... args) {
 
-            if (stack.size() < 2) {
-                logError("Ошибка: недостаточно элементов в стеке для операции +");
-                throw new CommandExecutionException("Недостаточно элементов в стеке для операции +");
-            }
-            double result = stack.pop() + stack.pop();
-            stack.push(result);
-            logInfo("Результат операции + : "  + result);
-        } catch (CommandExecutionException e) {
-//            logExeption(e);
-//            throw e;
-        } catch (Exception e) {
-            logExeption(e);
-            throw new RuntimeException("Неизвсетная ошибка во время выполнения",e);
+        if (context.getStack().size() < 2) {
+            logError("Ошибка: " +
+                    "недостаточно элементов в стеке для операции +");
+            throw new CommandExecutionException(
+                    "Недостаточно элементов в стеке для операции +");
         }
+
+        double result = context.getStack().pop() + context.getStack().pop();
+        context.getStack().push(result);
+
+        logInfo("Результат операции + : "  + result);
     }
 }
 
